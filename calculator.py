d852e824eb264b3377d9ac21960de13046e7a316
licpok <1326819896@qq.com>
2026-09-17 21:33:10 +0800

    学习了scale

--- calculator.py
+++ calculator.py
@@ -4,28 +4,26 @@ window = tk.Tk()
 window.title("my window")
 window.geometry("200x200")
 
-var = tk.StringVar()
-l = tk.Label(window, bg="yellow", width=20, text="empty")
+l = tk.Label(window, bg="yellow", width=30, text="empty")
 l.pack()
 
 
-def print_selection():
-    l.config(text="you have selected" + var.get())
+def print_selection(v):
+    l.config(text="you have selected " + v)
 
 
-r1 = tk.Radiobutton(
-    window, text="Option A", variable=var, value="A", command=print_selection
+s = tk.Scale(
+    window,
+    label="try me",
+    from_=5,
+    to=11,
+    orient=tk.HORIZONTAL,
+    length=200,
+    showvalue=0,
+    tickinterval=3,
+    resolution=0.01,
+    command=print_selection,
 )
-r1.pack()
-
-r2 = tk.Radiobutton(
-    window, text="Option B", variable=var, value="B", command=print_selection
-)
-r2.pack()
-
-r3 = tk.Radiobutton(
-    window, text="Option C", variable=var, value="C", command=print_selection
-)
-r3.pack()
+s.pack()
 
 window.mainloop()
