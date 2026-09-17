1edbc46f2f88cccc981944977525a242450fc73b
licpok <1326819896@qq.com>
2026-09-17 21:42:25 +0800

    学习了checkbutton

--- calculator.py
+++ calculator.py
@@ -8,22 +8,33 @@ l = tk.Label(window, bg="yellow", width=30, text="empty")
 l.pack()
 
 
-def print_selection(v):
-    l.config(text="you have selected " + v)
+def print_selection():
+    if (var1.get() == 1) & (var2.get() == 0) & (var3.get() == 0):
+        l.config(text="I only love Python!")
+    elif (var1.get() == 0) & (var2.get() == 1) & (var3.get() == 0):
+        l.config(text="I only love C++!")
+    elif (var1.get() == 0) & (var2.get() == 0) & (var3.get() == 1):
+        l.config(text="I only love JAVA!")
+    elif (var1.get() == 0) & (var2.get() == 0) & (var3.get() == 0):
+        l.config(text="I don't like anything!")
+    else:
+        l.config(text="I love computer!")
 
 
-s = tk.Scale(
-    window,
-    label="try me",
-    from_=5,
-    to=11,
-    orient=tk.HORIZONTAL,
-    length=200,
-    showvalue=0,
-    tickinterval=3,
-    resolution=0.01,
-    command=print_selection,
+var1 = tk.IntVar()
+var2 = tk.IntVar()
+var3 = tk.IntVar()
+c1 = tk.Checkbutton(
+    window, text="Python", variable=var1, onvalue=1, offvalue=0, command=print_selection
 )
-s.pack()
+c2 = tk.Checkbutton(
+    window, text="C++", variable=var2, onvalue=1, offvalue=0, command=print_selection
+)
+c3 = tk.Checkbutton(
+    window, text="JAVA", variable=var3, onvalue=1, offvalue=0, command=print_selection
+)
+c1.pack()
+c2.pack()
+c3.pack()
 
 window.mainloop()
