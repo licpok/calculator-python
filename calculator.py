0e63ebead86b545f010435ddfd980b88fbdf5aa5
licpok <1326819896@qq.com>
2026-09-20 10:55:28 +0800

    用python从零实现计算器（tkinter版）

--- calculator.py
+++ calculator.py
@@ -1,40 +1,46 @@
 import tkinter as tk
+import pickle
 
 window = tk.Tk()
-window.title("my window")
-window.geometry("200x200")
+window.title("Welcome to Xiaomuji's world")
+window.geometry("450x300")
 
-l = tk.Label(window, bg="yellow", width=30, text="empty")
-l.pack()
+canvas = tk.Canvas(window, height=200, width=500)
+image_file = tk.PhotoImage(file="welcome.gif")
+image = canvas.create_image(0, 0, anchor="nw", image=image_file)
+canvas.pack(side="top")
+
+tk.Label(window, text="user name").place(x=50, y=150)
+tk.Label(window, text="password").place(x=50, y=200)
+
+var_usr_name = tk.StringVar()
+var_usr_name.set("example@python.com")
+var_password = tk.StringVar()
+entry_usr_name = tk.Entry(window, textvariable=var_usr_name)
+entry_password = tk.Entry(window, textvariable=var_password, show="*")
+entry_usr_name.place(x=160, y=150)
+entry_password.place(x=160, y=200)
 
 
-def print_selection():
-    if (var1.get() == 1) & (var2.get() == 0) & (var3.get() == 0):
-        l.config(text="I only love Python!")
-    elif (var1.get() == 0) & (var2.get() == 1) & (var3.get() == 0):
-        l.config(text="I only love C++!")
-    elif (var1.get() == 0) & (var2.get() == 0) & (var3.get() == 1):
-        l.config(text="I only love JAVA!")
-    elif (var1.get() == 0) & (var2.get() == 0) & (var3.get() == 0):
-        l.config(text="I don't like anything!")
-    else:
-        l.config(text="I love computer!")
+def usr_login():
+    usr_name = var_usr_name.get()
+    usr_password = var_password.get()
+    try:
+        with open("usrs_info.pickle", "rb") as usr_file:
+            usrs_info = pickle.load(usr_file)
+    except FileNotFoundError:
+        with open("usrs_info.pickle", "wb") as usr_file:
+            usrs_info = {"admin": "admin"}
+            pickle.dump(usrs_info, usr_file)
 
 
-var1 = tk.IntVar()
-var2 = tk.IntVar()
-var3 = tk.IntVar()
-c1 = tk.Checkbutton(
-    window, text="Python", variable=var1, onvalue=1, offvalue=0, command=print_selection
-)
-c2 = tk.Checkbutton(
-    window, text="C++", variable=var2, onvalue=1, offvalue=0, command=print_selection
-)
-c3 = tk.Checkbutton(
-    window, text="JAVA", variable=var3, onvalue=1, offvalue=0, command=print_selection
-)
-c1.pack()
-c2.pack()
-c3.pack()
+def usr_sign_up():
+    pass
+
+
+btn_login = tk.Button(window, text="login", command=usr_login)
+btn_sign_up = tk.Button(window, text="sign up", command=usr_sign_up)
+btn_login.place(x=170, y=230)
+btn_sign_up.place(x=270, y=230)
 
 window.mainloop()
